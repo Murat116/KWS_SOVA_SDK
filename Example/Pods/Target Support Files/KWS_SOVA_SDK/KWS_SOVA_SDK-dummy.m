89ae78509384157836fd0345c2d487ac60f5940c
Murat <89196242960@mail.ru>
2021-04-09 15:16:28 +0300

    version 1.0.0

---
+++ Example/Pods/Target Support Files/KWS_SOVA_SDK/KWS_SOVA_SDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KWS_SOVA_SDK : NSObject
+@end
+@implementation PodsDummy_KWS_SOVA_SDK
+@end
---
+++ Example/Pods/Target Support Files/KWS_SOVA_SDK/KWS_SOVA_SDK-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double KWS_SOVA_SDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char KWS_SOVA_SDKVersionString[];
+
